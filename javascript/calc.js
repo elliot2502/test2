0d9fefcd4994feafa32297815601befe9228f6cc
Elliot Small <elliotcsmall@hotmail.com>
2020-04-26 14:56:29 +0100

    initial commit 26/04/20

---
+++ javascript/calc.js
@@ -0,0 +1,27 @@
+
+
+function calc () {
+    const w =document.getElementById("widthR").value;
+const h =document.getElementById("lengthR").value;
+const d =document.getElementById("heightR").value;
+// const unit =document.getElementById("sqm").value;
+let unit = 0;
+if (document.getElementById("sqm").checked) {
+    unit = document.getElementById("sqm").value ;
+
+} else if (document.getElementById("sqft").checked) {
+   unit = document.getElementById("sqft").value ;
+
+}
+
+if (w==="" || h==="" || d==="") {
+    alert("You need a value for each field")
+}
+var result =(w * h * d) ;
+
+
+
+    document.getElementById("answertxt").value = (result+ " "+ unit);
+   
+
+}
